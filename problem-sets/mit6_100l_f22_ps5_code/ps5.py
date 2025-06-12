aa5d34b09daf5aa0fe5902b1d82bc7dbde9b0d6e
Braulio Rocha <rochabrc@gmail.com>
2025-06-12 16:13:19 +0100

    added reveal_color_image function

--- problem-sets/mit6_100l_f22_ps5_code/ps5.py
+++ problem-sets/mit6_100l_f22_ps5_code/ps5.py
@@ -164,7 +164,6 @@ def reveal_bw_image(filename):
     # Extracts pixel information hidden on LSB
     for px in pixels:
         hidden_pixels.append(extract_end_bits(1, px)*255)
-    print(hidden_pixels)
     # Gets the size of original image
     with Image.open(filename) as img:
         img_size = img.size
@@ -181,7 +180,25 @@ def reveal_color_image(filename):
     Returns:
         result: an Image object containing the hidden image
     """
-    pass
+    pixels = img_to_pix(filename)
+    hidden_pixels_unscaled = []
+    # Extracts pixel information hidden on LSB
+    for px in pixels:
+        hidden_pixels_unscaled.append(extract_end_bits(3, px))
+    hidden_pixels_unscaled_arr = numpy.array(hidden_pixels_unscaled)
+    # Scales hidden pixels to full 8-bit range
+    hidden_pixels_scaled_arr = hidden_pixels_unscaled_arr*(255/7)
+    numpy.round(hidden_pixels_scaled_arr,0,hidden_pixels_scaled_arr)
+    hidden_pixels_scaled_arr_1 = hidden_pixels_scaled_arr.astype(int)
+    hidden_pixels = hidden_pixels_scaled_arr_1.tolist()
+    # Convert elements from hidden_pixels back to tuples for pix_to_img func
+    for i in range(len(hidden_pixels)):
+        hidden_pixels[i] = tuple(hidden_pixels[i])
+    # Gets the size of original image
+    with Image.open(filename) as img:
+        img_size = img.size
+    # Creates image obj from pixel information and size
+    return pix_to_img(hidden_pixels, img_size, 'RGB')
 
 
 def reveal_image(filename):
@@ -228,9 +245,9 @@ def main():
 
     # Uncomment the following lines to test part 1
 
-    im = Image.open('image_15.png')
-    width, height = im.size
-    pixels = img_to_pix('image_15.png')
+    # im = Image.open('image_15.png')
+    # width, height = im.size
+    # pixels = img_to_pix('image_15.png')
 
     # non_filtered_pixels = filter(pixels,'none')
     # im = pix_to_img(non_filtered_pixels, (width, height), 'RGB')
@@ -241,11 +258,12 @@ def main():
     # im2.show()
 
     # Uncomment the following lines to test part 2
-    im = reveal_image('hidden1.bmp')
-    #im.save('hidden1_decoded.bmp)
-    im.show()
+    # im = reveal_image('hidden1.bmp')
+    # im.save('hidden1_decoded.bmp')
+    # im.show()
 
-    #im2 = reveal_image('hidden2.bmp')
+    # im2 = reveal_image('hidden2.bmp')
+    # im2.save('hidden2_decoded.bmp')
     # im2.show()
     
 
